b1ba7ed37ec6f6c1884db72facf713d80da36250
lakshmi-tech07 <shreelakshmi.vittal@invenger.com>
2026-04-10 16:31:05 +0530

    employee.js added

---
+++ employee.js
@@ -0,0 +1,32 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const Employee = sequelize.define('Employee', {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4, // auto generate UUID
+    primaryKey: true
+  },
+  empName: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  empEmail: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  designation: {
+    type: DataTypes.STRING
+  },
+  joiningDate:{
+    type:DataTypes.DATE,
+    allowNull: true
+  },
+  status: {
+    type: DataTypes.STRING,
+    defaultValue: 'Active'
+  }
+});
+
+module.exports = Employee;
